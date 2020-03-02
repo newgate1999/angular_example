ad765af53f6a0b7470ba099c28cb8af477afb0a9
newgate <tuananh301099@gmail.com>
2020-03-02 15:59:13 +0700

    angular_example

---
+++ src/app/products.ts
@@ -0,0 +1,21 @@
+export const products = [
+  {
+    name: 'Iphone 11 ProMax',
+    price: 1000,
+    description: 'Color: Grey, Capacity: 256Gh',
+    image: '../assets/images.jpeg'
+  },
+  {
+    name: 'IPhone 11',
+    price: 799,
+    description: 'Color: silver, Capacity: 128Gh',
+    image: '../assets/download (1).jpeg'
+  },
+  {
+    name: 'Iphone XS MAX',
+    price: 899,
+    description: 'Color: gold, Capacity: 256Gh',
+    image: '../assets/download.jpeg'
+  }
+];
+
